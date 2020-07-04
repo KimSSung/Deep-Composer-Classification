6c8ae9c3f9a2aeb32c31953a99c96d5e324b2c2f
KimSSung <hahala25@naver.com>
2020-07-04 11:18:53 +0000

    remove no use folders

--- no_use/dropna.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-
-genres = ['Classical', 'Jazz', 'Pop', 'Rock', 'Country']
-for genre in genres:
-	load_df = pd.read_pickle('./pickles/'+ genre + '.pickle')
-	new_df = load_df.dropna(how = "any")
-	new_df.to_pickle('./pickles/D_' + genre + '.pickle')
