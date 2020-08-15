fc51d2b92906eed42c6a2603c06d76b457c18d47
KimSSung <hahala25@naver.com>
2020-08-15 18:39:12 +0000

    trivial

--- tools/data_loader.py
+++ tools/data_loader.py
@@ -13,7 +13,6 @@ class MIDIDataset(Dataset):
         self, txt_file, classes=13, omit=None, seg_num=20, age=False, transform=None
     ):
         self.txt_file = txt_file
-        self.classes = classes
         self.classes = [x for x in range(classes)]
 
         self.seg_num = seg_num  # seg num per song
@@ -32,7 +31,7 @@ class MIDIDataset(Dataset):
 
         txt_list = open(self.txt_file, "r")
         for midi_pth in txt_list:  # each midi
-            label = midi_pth[midi_pth.find("composer") + 8]
+            label = midi_pth[midi_pth.find("composer") + len("composer")]
             ver_npy = glob(midi_pth.replace("\n", "") + "*.npy")  # list
             # randomly select n segments pth
             tmp = [random.choice(ver_npy) for j in range(self.seg_num)]
