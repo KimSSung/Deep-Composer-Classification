f9ddf3b7383485675deebec05feeacf2812b6311
KimSSung <hahala25@yonsei.ac.kr>
2020-09-21 16:16:27 +0000

    bar chart

--- tools/confusion_matrix.py
+++ tools/confusion_matrix.py
@@ -22,11 +22,11 @@ from tools.data_loader import MIDIDataset
 from models.wresnet import resnet18, resnet34, resnet101, resnet152, resnet50
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 
 class ConfusionMatrix:
-    def __init__(self, label_num=13, seg_num=90, sort=True, normalize=True, bar=False):
+    def __init__(self, label_num=13, seg_num=90, sort=True, normalize=True, bar=True, barmode=None):
 
         self.label_num = label_num
         self.seg_num = seg_num
@@ -36,7 +36,8 @@ class ConfusionMatrix:
 
         self.sort = sort
         self.normalize = normalize
-        self.bar = bar
+        self.bar = bar # T of F
+        self.barmode = barmode # 'age' or 'data' or None
 
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.model = resnet50(2, self.label_num)
@@ -241,32 +242,51 @@ class ConfusionMatrix:
         print(">>> Drawing bar graph....")
         # no sorting for bar graph
         dic = classification_report(true, pred, target_names=self.axis_labels, output_dict=True)
-        
+        print(dic)
         bar_values = []
         for i in range(self.label_num):
             # print(dic[self.axis_labels[i]]['f1-score'])
-            bar_values.append(round(dic[self.axis_labels[i]]['f1-score'], 2))
+            bar_values.append(dic[self.axis_labels[i]]['f1-score'])
+            print(self.axis_labels[i] + " -> " + str(dic[self.axis_labels[i]]['f1-score']))
+
+
+        # sorting by age or # of data
+        if self.barmode == 'age' or self.barmode == 'data':
+            want_order = []
+            if self.barmode == 'age':
+                want_order = [2, 6, 4, 8, 9, 12, 0, 1, 3, 5, 7, 10, 11]
+            else: # self.barmode == 'data'
+                want_order = [9, 4, 3, 5, 1, 12, 11, 8, 10, 7, 6, 0, 2]
+            
+            b = np.array(bar_values)
+            bar_values = b[want_order]
+            self.axis_labels = self.axis_labels[want_order]
+            print('bar vals:', bar_values)
+            print('labels:', self.axis_labels)
+        else: # self.barmode == None
+            self.barmode = '' # for save pdf name
+
 
         indices = range(len(bar_values))
 
-        fig, ax = plt.subplots(figsize=(12,9))
+        fig, ax = plt.subplots(figsize=(21,11))
         ax.bar(indices, bar_values, width=0.5, align="center")
         for i in range(len(bar_values)):
-            plt.annotate('{0:03.3f}'.format(bar_values[i]).lstrip('0'), (-0.5 + i, bar_values[i] + 0.01), fontsize=20)
+            plt.annotate('{0:03.3f}'.format(bar_values[i]).lstrip('0'), (-0.47 + i, bar_values[i] + 0.01), fontsize=30)
         ax.spines['top'].set_visible(False)
         ax.spines['right'].set_visible(False)
         ax.spines['bottom'].set_linewidth(False)
         ax.spines['left'].set_linewidth(False)
-        plt.xticks(indices, self.axis_labels, fontsize=20, rotation=-45)
-        plt.setp(ax.get_yticklabels(), fontsize=20)
+        plt.xticks(indices, self.axis_labels, fontsize=30, rotation=45)
+        plt.setp(ax.get_yticklabels(), fontsize=30)
         plt.ylim([0.5, 1.01])
 
         # plt.title("Bar chart, with normalization", fontsize=12)
-        # plt.xlabel("composers", fontsize=20)
-        plt.ylabel("F1 score", fontsize=20)
+        # plt.xlabel("composers", fontsize=15)
+        plt.ylabel("F1 score", fontsize=40)
         plt.grid(b=True, linewidth=0.1, axis='y')
         # plt.subplots_adjust(left=0.125, right=0.9, bottom=0.1, top=0.9, wspace=0.2, hspace=0.2)
-        plt.savefig("barchart.pdf", dpi=1000)
+        plt.savefig("barchart_" + self.barmode + ".pdf", dpi=1000)
 
 
 
@@ -275,7 +295,7 @@ if __name__ == "__main__":
 
     # for base train
 
-    temp = ConfusionMatrix(label_num=13, seg_num=90, sort=True, normalize=True, bar=True)
+    temp = ConfusionMatrix(label_num=13, seg_num=90, sort=True, normalize=True, bar=True, barmode='data')
     temp.run()
 
 # for attacker: only generate matrix example
